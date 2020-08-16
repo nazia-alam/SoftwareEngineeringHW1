b83be52a215ca64305c5d2b7ba63d6a4565c24e1
hzhang934748656 <44934005+hzhang934748656@users.noreply.github.com>
2020-08-16 09:26:53 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="hzhang934748656",
+    version="0.0.1",
+    author="SE20 Group 10",
+    author_email="hzhang62@ncsu.edu",
+    description="This is Group 10 homework 1 for CS510 Software Engineering",
+    url="https://github.com/nazia-alam/SoftwareEngineeringHW1",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
